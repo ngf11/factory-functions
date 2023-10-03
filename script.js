e687e33e3bb30659eb8eeb00d51238c972301849
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-03 18:54:01 -0400

    Inheritance with factories

--- script.js
+++ script.js
@@ -1,30 +1,66 @@
-// Closures
-function human(name) {
-  function sayHi() {
-    console.log(`Hello, my name is ${name}`);
-  }
-  function howYouFeel() {
-    console.log(`${name} feeling Good!`);
-  }
-  return {
-    sayHi,
-    howYouFeel,
-  };
-}
-human("nico");
-const nico = human("nico");
-const didi = human("Didi wu TinTin");
-didi.sayHi();
-didi.howYouFeel();
+const factoryFunctions = (string) => {
+  const capString = () => string.toUpperCase();
+  const printString = () => console.log(`---${capString()}---`);
+  return { printString };
+};
 
-//  module
-var Module = (function () {
-  return {
-    myMethod: function () {
-      console.log("myMethod has been called.");
-    },
-  };
-})();
+const milo = factoryFunctions("Milo");
+milo.printString();
 
-// call module + methods
-Module.myMethod();
+const miloFactori = (string) => {
+  const capMilo = () => string.toUpperCase().concat(" FAT");
+  const printMilo = () => {
+    console.log(`<<<<< ${capMilo()} >>>>>`);
+  };
+  return { printMilo };
+};
+
+const nico = miloFactori("nico");
+
+nico.printMilo();
+
+const counterCreator = () => {
+  let count = 0;
+  return () => {
+    console.log(count);
+    count++;
+  };
+};
+const counter = counterCreator();
+
+counter();
+counter();
+counter();
+counter();
+counter();
+
+const Player = (name, level) => {
+  let health = level * 2;
+  const getLevel = () => level;
+  const getName = () => name;
+  const die = () => {
+    console.log("GAME OVER");
+  };
+  const damage = (x) => {
+    health -= x;
+    if (health <= 0) {
+      die();
+    }
+  };
+  const attack = (enemy) => {
+    if (level < enemy.getLevel()) {
+      damage(1);
+      console.log(`${enemy.getName()} has damaged ${name}`);
+    }
+    if (level >= enemy.getLevel()) {
+      enemy.damage(1);
+      console.log(`${name} has damaged ${enemy.getName()}`);
+    }
+  };
+  return { attack, damage, getLevel, getName };
+};
+
+const jimmie = Player("jim", 10);
+const badGuy = Player("jeff", 1);
+jimmie.attack(badGuy);
+jimmie.attack(badGuy);
