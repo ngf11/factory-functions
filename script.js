56522074488b7326c4b451635f9f4b23788d0ea9
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-03 06:56:18 -0400

    Closures and module

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+// Closures
+function human(name) {
+  function sayHi() {
+    console.log(`Hello, my name is ${name}`);
+  }
+  function howYouFeel() {
+    console.log(`${name} feeling Good!`);
+  }
+  return {
+    sayHi,
+    howYouFeel,
+  };
+}
+human("nico");
+const nico = human("nico");
+const didi = human("Didi wu TinTin");
+didi.sayHi();
+didi.howYouFeel();
 
+//  module
+var Module = (function () {
+  return {
+    myMethod: function () {
+      console.log("myMethod has been called.");
+    },
+  };
+})();
+
+// call module + methods
+Module.myMethod();
