0ef86478ad1fd9a0c0a73df5ae1bfbef465ac265
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-06 06:46:36 -0400

    Object.assign() functions mixins

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 // Object.assign() functions mixins
+const humanFactory = function (obj) {
+  let isCrying = false;
+
+  return Object.assign({}, obj, {
+    cry() {
+      isCrying = true;
+      return this;
+    },
+    isCrying() {
+      return isCrying;
+    },
+  });
+};
+
+const flyFactory = function (obj) {
+  let isflying = false;
+
+  return Object.assign({}, obj, {
+    fly() {
+      isflying = true;
+      return this;
+    },
+    isflying() {
+      return isflying;
+    },
+  });
+};
+
+const superman = humanFactory(flyFactory({}));
+console.log(superman.fly().cry().isCrying());
