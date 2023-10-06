6b8377e90a7ee507c6991e416a58184421093eef
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-06 10:30:47 -0400

    class-free objects and inheritance using prototypes, and concatenation

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
-// Object.assign() functions mixins
-const humanFactory = function (obj) {
-  let isCrying = false;
-
-  return Object.assign({}, obj, {
-    cry() {
-      isCrying = true;
-      return this;
-    },
-    isCrying() {
-      return isCrying;
-    },
-  });
+const proto = {
+  hello: function () {
+    return `Hi my name is ${this.name}`;
+  },
+  age: function () {
+    return `I am ${this.years} years old`;
+  },
 };
 
-const flyFactory = function (obj) {
-  let isflying = false;
-
-  return Object.assign({}, obj, {
-    fly() {
-      isflying = true;
-      return this;
-    },
-    isflying() {
-      return isflying;
-    },
-  });
+const animal = {
+  greet: function () {
+    return `I am a ${this.species}`;
+  },
 };
 
-const superman = humanFactory(flyFactory({}));
-console.log(superman.fly().cry().isCrying());
+const nico = Object.create(proto);
+for (const key in animal) {
+  if (animal.hasOwnProperty(key)) {
+    nico[key] = animal[key];
+  }
+}
+nico.name = "nico";
+nico.years = 34;
+nico.species = "Neanderthal";
+console.log(nico.hello());
+console.log(nico.age());
+console.log(nico.greet());
+const milo = Object.assign({}, proto, {
+  name: "Milo",
+  years: 10,
+
+  say() {
+    return "Woof";
+  },
+});
+console.log(milo.age());
+console.log(milo.hello());
+console.log(milo.say());
+
+const me = "Nico";
+
+function herro() {
+  console.log(`Herro it me ` + me);
+}
+herro();
+console.log(nico);
