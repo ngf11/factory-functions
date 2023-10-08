abc0ae0fa85121762ed942123e4d812f0779bc42
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-08 16:25:23 -0400

    closures

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//The module pattern
+//closures
+
+function outer() {
+  const outerVar = "Hey I am an outer var";
+  return function inner() {
+    const innerVar = "Hey I am an inner var";
+    console.log(innerVar);
+    console.log(outerVar);
+  };
+}
