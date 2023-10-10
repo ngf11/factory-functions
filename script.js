125b8c996ece27cfc096032f209664604c1d33f7
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-10 13:24:23 -0400

    Factory Functions

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//Constructors
+//Factory Functions
+
+function factoryFunction(str) {
+  const obj = Object.create(factoryFunction.prototype);
+  obj.str = str;
+  return obj;
+}
+const nico = factoryFunction("nico");
+console.log(nico);
+
+// const Formatter = (function () {
+//   const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
+// })();
+// const Formatter = (function () {
+//   console.log("Start");
+//   const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
+// })();
