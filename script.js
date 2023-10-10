6347be19efdcc3ace6d1a0f9bef1909dbb30520f
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-10 13:57:15 -0400

    Modular

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-//Factory Functions
-
-function factoryFunction(str) {
-  const obj = Object.create(factoryFunction.prototype);
-  obj.str = str;
-  return obj;
-}
-const nico = factoryFunction("nico");
-console.log(nico);
-
-// const Formatter = (function () {
-//   const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
-// })();
-// const Formatter = (function () {
-//   console.log("Start");
-//   const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
-// })();
+//objewct literal
+const myModual = {
+  name: "nico",
+  age: 34,
+  sayName() {
+    console.log(` my name is ${this.name}`);
+  },
+  setName(newName) {
+    this.name = newName;
+  },
+};
