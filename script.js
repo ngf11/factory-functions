0351e8e8bf9ecc957156aba3be41898011b35c87
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-20 19:43:49 -0400

    factory

--- script.js
+++ script.js
@@ -1,36 +1,13 @@
-function createUser(name) {
-  const discoreName = "@" + name;
-  let reputation = 0;
-  const getReputation = () => reputation;
-  const giveReputation = () => reputation++;
-  return {
-    name,
-    discoreName,
-    getReputation,
-    giveReputation,
-  };
-}
-const nico = createUser("nico");
-nico.giveReputation();
-nico.giveReputation();
-console.log(nico);
+const thing = {
+  dna: Math.random(),
+};
+const obj = Object.create(thing);
+console.log(obj);
+console.log(obj.dna);
 
-console.log({
-  discordName: nico.discoreName,
-  reputation: nico.getReputation(),
+const obj2 = Object.create({});
+Object.defineProperty(obj2, "milo", {
+  value: "is fat",
 });
-// Prototypal inheritance with factories
-function createPlayer(name, level) {
-  const { discoreName, getReputation } = createUser(name);
-  const increseLevel = () => level++;
-  return { name, discoreName, getReputation, increseLevel };
-}
-
-const milo = createPlayer("milo", 3);
-milo.increseLevel();
-milo.increseLevel();
-console.log({
-  name: milo.name,
-  discoreName: milo.discoreName,
-  level: milo.increseLevel(),
-});
+console.log(obj2);
+console.log(obj2.milo);
