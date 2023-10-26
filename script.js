0ba86ed899c216e358c1bd57d20aca6601fb469a
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-26 11:44:58 -0400

    Object Constractor

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-const thing = {
-  dna: Math.random(),
+function Person(name, age, city) {
+  this.name = name;
+  this.age = age;
+  this.city = city;
+}
+Person.prototype.greet = function () {
+  console.log(`Hi, I am ${this.name}`);
 };
-const obj = Object.create(thing);
-console.log(obj);
-console.log(obj.dna);
+Person.prototype.form = function () {
+  console.log(`I am from ${this.city}`);
+};
 
-const obj2 = Object.create({});
-Object.defineProperty(obj2, "milo", {
-  value: "is fat",
-});
-console.log(obj2);
-console.log(obj2.milo);
+const nico = new Person("nico", 34, "Montevideo");
+console.log(nico);
+console.log(Person.prototype);
+console.log("-------");
+console.log(Object.getPrototypeOf(nico) === Person.prototype);
+
+function UFO(name, age, city, planet) {
+  Person.call(this, name, age, city);
+  this.planet = planet;
+}
+
+UFO.prototype.live = function () {
+  console.log(`I live in ${this.planet}`);
+};
+Object.setPrototypeOf(UFO.prototype, Person.prototype);
+
+const elon = new UFO("elon", 50, "A galaxy far far away", "Mars");
+console.log(Object.getPrototypeOf(UFO.prototype));
+nico.greet();
+nico.form();
+console.log("-----");
+elon.greet();
+elon.live();
+elon.form();
